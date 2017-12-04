1835945015ec176b7f0be10d9e6e4a77a37a08ca
Chan Kang <kentakang@icloud.com>
2017-12-04 10:13:28 +0900

    Add loop.py
    Add loop and list

---
+++ loop.py
@@ -0,0 +1,24 @@
+﻿for i in range(10):
+	print("HeLlO")
+
+for i in range(10):
+	print(i)
+
+for i in range(1, 11):
+	print(i)
+
+for i in range(2, 11, 2):
+	print(i)
+
+list = ['Secret', 'Energy', 'X-ray']
+for i in list:
+	print(i)
+
+numbers = range(10)
+for i in numbers:
+	print(i)
+
+sum = 0
+for i in range(1, 11):
+	sum += i
+print(sum)
